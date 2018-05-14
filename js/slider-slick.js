3a22500de738fba9bb211c7bf458710d406d0dbb
Miguel Moreno <miguel@concepthaus.mx>
2018-05-13 22:01:50 -0500

    primer commit

---
+++ js/slider-slick.js
@@ -0,0 +1,92 @@
+$(document).on('ready', function() {
+    $(".vertical-center-4").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 2
+    });
+    $(".vertical-center-3").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+    $(".vertical-center-2").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+      slidesToShow: 2,
+      slidesToScroll: 2
+    });
+    $(".vertical-center").slick({
+      dots: true,
+      vertical: true,
+      centerMode: true,
+    });
+    $(".vertical").slick({
+      dots: true,
+      vertical: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+    $(".regular").slick({
+      dots: true,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+    $(".center").slick({
+      dots: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 5,
+      slidesToScroll: 3
+    });
+    $(".variable").slick({
+      dots: true,
+      infinite: true,
+      variableWidth: true
+    });
+    $(".lazy").slick({
+      lazyLoad: 'ondemand', // ondemand progressive anticipated
+      infinite: true
+    });
+    $('.responsive').slick({
+dots: true,
+infinite: false,
+speed: 300,
+slidesToShow: 4,
+slidesToScroll: 4,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+              
+  });
