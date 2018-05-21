885ed80f7963668e82eb36e629b4574091ffb65e
MIGUEL ANGEL MORENO <you@example.com>
2018-05-21 14:05:10 -0500

    se anclaron las post se la magazine en pagina web

--- js/controllers/app.js
+++ js/controllers/app.js
@@ -2,8 +2,8 @@ var myApp = angular.module('app', ['ngSanitize', 'ngMask']);
 
 myApp.controller('entradas', ['$scope','$window','$http', function($scope,$window,$http) {
 
-  $http.get('http://admin.concepthaus.mx/hgcorp/blog/wp-json/wp/v2/posts/?_embed', {
-    headers: {'Authorization': 'Basic '+btoa('adminch:daemon_systemd010')}
+  $http.get('http://www.jbglobalgroup.com/wp-json/wp/v2/posts/?_embed', {
+    headers: {'Authorization': 'Basic '+btoa('glo_JB_al:Magazine_Global_Mkt')}
   }).then(function (res){
     $scope.postConsulta = res.data;
     console.log($scope.postConsulta);
